a2f4a210217842cec0385d3207025dcb8d1b9155
frankie-11 <frank15ortiz@aol.com>
2022-12-21 17:04:23 -0500

    changes

--- frontend/main.js
+++ frontend/main.js
@@ -1,24 +1,24 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    getVisitCounter();
-})
+    getVisitCount();
+});
 
-const functionApiUrl = "https://azureresumefrankie.azurewebsites.net/api/GetResumeCounter?code=8A8mAQbGZIk9TgQZDl8f8XxSSm6N6AGkA65kvCktKbmFAzFuelsfKA==";
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
- const getVisitCounter = () => {
+const localApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApi = 'https://azureresumefrankie.azurewebsites.net/api/GetResumeCounter?code=8A8mAQbGZIk9TgQZDl8f8XxSSm6N6AGkA65kvCktKbmFAzFuelsfKA=='; 
 
+const getVisitCount = () => {
     let count = 30;
-
-    fetch(functionApiUrl).then(response => {
-
+    fetch(functionApi)
+    .then(response => {
         return response.json()
-    }).then(response => {
-        console.log("Counter API has been trigerred.");
+    })
+    .then(response => {
+        console.log("Website called function API.");
         count = response.count;
-        document.getElementById("counter").innerText = count;
-    }).catch(function(erro){
+        console.log(document.getElementById('counter').innerText);
+        document.getElementById('counter').innerText = count;
+    }).catch(function(error) {
         console.log(error);
-    });
+      });
     return count;
-
- }
+}
